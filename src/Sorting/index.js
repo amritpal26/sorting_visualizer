5d35496144a11ed51b6807e133e6e657afcaaa5e
Amritpal Singh <38362080+amritpal26@users.noreply.github.com>
2021-02-01 18:22:11 -0800

    Delete src/Sorting directory
    delete empty code

--- src/Sorting/index.js
+++
@@ -1,9 +0,0 @@
-    InsertionSort: insertionSort,
-    HeapSort: heapSort
-    InsertionSort: insertionSort,
-    HeapSort: heapSort
-    HEAP_SORT: "heap_sort",
-}
-    InsertionSort: insertionSort,
-    HeapSort: heapSort
-}
